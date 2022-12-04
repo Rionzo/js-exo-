91779e1964ac66c3d72e114357836f982769363c
LIGUORI <tonyliguori24@gmail.com>
2022-12-04 18:25:30 +0100

    fini avec innerWidth

--- exo3/script.js
+++ exo3/script.js
@@ -3,11 +3,8 @@ function myMove() {
   var pos = 0;
   var id = setInterval(frame, 5);
   function frame() {
-    if (pos == 750) {
+    if (pos == window.innerWidth) {
       clearInterval(id);
-      setTimeout(function () {
-        element.remove();
-      }, 500);
     } else {
       pos++;
       element.style.left = pos + "px";
